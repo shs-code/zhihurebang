5a7330fd8ee0e12a070db414eeeeec2c2384bf21
shs-code <69229531+shs-code@users.noreply.github.com>
2020-08-05 12:04:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ main_page = BeautifulSoup(text,"html.parser")
 table = main_page.find("div",{"class":"HotList-list"})  #把想要的找到
 
 title = table.find("h2",{"class":"HotItem-title"}).text #标题
+print(title)
 excerpt = table.find('p', class_='HotItem-excerpt').text #题目内容
+print(excerpt)
 website = table.find('a').get('href')             #网址
+print(website)
 hot = table.find('div',class_="HotItem-metrics HotItem-metrics--bottom").text.replace('\u200b分享','') #热度，去掉'分享'
+print(hot)
 
+#没问题就写个批量
 datai = []
 n=0
 for i in table:
@@ -32,5 +37,9 @@ for i in table:
         pass
     datai.append(dic)
         # 分别获取字段内容
-print('成功采集%i条数据' % n)    
+    # print('成功采集%i条数据' % n)  在for循环内可以检查哪一条爬取失败  
+print('成功采集共%i条数据' % n)    
 datai[:2]
+
+df = pd.DataFrame(datai)
+ndf= df.fillna('如题') #有些题目内容是空白，即是题目标题
